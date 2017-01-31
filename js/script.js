f93fbde53b813212e22ea433f689678b893608e1
zaza <louizasdk@gmail.com>
2017-01-31 17:39:28 +0100

    ajout map + footer

---
+++ js/script.js
@@ -0,0 +1,2 @@
+var contact = document.getElementById("contact");
+contact.innerHTML = "<h4>"+contenu.baseline + "</h4> <p>"+ contenu.addresse +"</p> <p>"+ contenu.codePostale + " "+ contenu.ville + "</p>";
